d9ce90939f6d9573bfc00b77bac0eebe6f7a296c
nwilliams2u <104388848+nwilliams2u@users.noreply.github.com>
2022-06-16 17:52:53 +0100

    Highlight Sample Table
    Row and Column Sample Tables

---
+++ Highlight/script.js
@@ -0,0 +1,5 @@
+var mainTable = $('table');
+
+for (var i = 0; i < 10; i++) {
+  mainTable.find('tbody').append('<tr><td>Data 1</td><td>Data 2</td><td>Data 3</td><td>Data 4</td><td>Data 5</td></tr>')
+}
