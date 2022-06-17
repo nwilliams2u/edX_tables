9a41aa5903037ec11371a90f38ff97cc1a3282d3
nwilliams2u <104388848+nwilliams2u@users.noreply.github.com>
2022-06-17 12:52:50 +0100

    Adding Responsive table examples
    Responsive table examples

---
+++ Responsive _01/script.js
@@ -0,0 +1,5 @@
+var mainTable = $('.fl-table');
+
+for (var i = 0; i < 12; i++) {
+  mainTable.find('tbody').append('<tr><td>Data 1</td><td>Data 2</td><td>Data 3</td><td>Data 4</td><td>Data 5</td></tr>')
+}
